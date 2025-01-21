3de9840e52e7acffc4311b7e29cdea90d4de4174
chwangmin <ckm7907@naver.com>
2025-01-21 08:59:02 +0000

    fix: 게시판 정렬 해결
    - 오타 수정

--- be/board/src/main/java/com/first/board/domain/board/type/SortType.java
+++ be/board/src/main/java/com/first/board/domain/board/type/SortType.java
@@ -3,8 +3,8 @@ package com.first.board.domain.board.type;
 import org.springframework.data.domain.Sort;
 
 public enum SortType {
-    CREATED_DESC("createdDate", Sort.Direction.DESC),    // 등록날짜 최신순
-    CREATED_ASC("createdDate", Sort.Direction.ASC),     // 등록날짜 오래된순
+    CREATED_DESC("createDate", Sort.Direction.DESC),    // 등록날짜 최신순
+    CREATED_ASC("createDate", Sort.Direction.ASC),     // 등록날짜 오래된순
     VIEWS_DESC("views", Sort.Direction.DESC),      // 조회수 높은순
     VIEWS_ASC("views", Sort.Direction.ASC);        // 조회수 낮은순
 
