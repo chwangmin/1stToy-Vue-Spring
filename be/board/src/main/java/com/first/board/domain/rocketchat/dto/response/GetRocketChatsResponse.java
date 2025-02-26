49eba22dd9c0639ea98638f233cbe52eef92c97a
chwangmin <ckm7907@naver.com>
2025-02-26 09:43:21 +0900

    [BE] fix: response dto 에 @Getter 추가
    - No acceptable representation 에러 해결

--- be/board/src/main/java/com/first/board/domain/rocketchat/dto/response/GetRocketChatsResponse.java
+++ be/board/src/main/java/com/first/board/domain/rocketchat/dto/response/GetRocketChatsResponse.java
@@ -2,9 +2,11 @@ package com.first.board.domain.rocketchat.dto.response;
 
 import com.first.board.domain.rocketchat.dto.RocketChatDto;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @Builder
 public class GetRocketChatsResponse {
     private List<RocketChatDto> rocketChats;
